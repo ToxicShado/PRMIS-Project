277e545fb88ae45236e7a13d6496ae29e1bb0bb6
ToxicShado <toxicshadowns@gmail.com>
2025-01-09 22:10:46 +0100

    point 2 and 3

---
+++ Process Control/Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            // Create a UDP socket for initiating a connection
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 25565);
+
+            // Send a registration message to the server
+            byte[] buffer = new byte[1024];
+            string registrationMessage = "CouldYouPleaseConnect";
+            byte[] registrationData = Encoding.UTF8.GetBytes(registrationMessage);
+            udpSocket.SendTo(registrationData, serverEP);
+
+            // Create a TCP socket for the actual communication
+            Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint tcpServerEP = new IPEndPoint(IPAddress.Loopback, 0);
+
+            // Receive the IP and port of the TCP socket from the server
+            EndPoint serverResponseEndpoint = new IPEndPoint(IPAddress.Any, 0);
+            int receivedBytes = udpSocket.ReceiveFrom(buffer, ref serverResponseEndpoint);
+
+            udpSocket.Close();
+
+            // Parse the received data
+            string serverResponse = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+            string[] serverResponseParts = serverResponse.Split(',');
+            tcpServerEP.Address = IPAddress.Parse(serverResponseParts[0] == "0.0.0.0" ? "127.0.0.1" : serverResponseParts[0]);
+            tcpServerEP.Port = int.Parse(serverResponseParts[1]);
+            Console.WriteLine(serverResponse);
+
+            // Connect to the server with the provided data
+            tcpSocket.Connect(tcpServerEP);
+
+            // Test the connection by sending a message to the server
+            string message = "Hello from the client";
+            byte[] messageData = Encoding.UTF8.GetBytes(message);
+            tcpSocket.Send(messageData);
+            tcpSocket.Close();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Process Control/Server/Server.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        public static void Main(string[] args)
+        {
+            double procesorState = 0;
+            double memoryState = 0;
+
+            Console.WriteLine("Pick whether you wish to use Round Robin (1) or to sort by priority (2)");
+            int choice = -1;
+            int.TryParse(Console.ReadLine(), out choice);
+            Console.WriteLine($"Choice {choice}");
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Round Robin");
+                    break;
+                case 2:
+                    Console.WriteLine("Priority");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
+            // Create a UDP socket for initiating a connection
+            Socket initialConnection = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 25565);
+            initialConnection.Bind(serverEP);
+
+            // Create a TCP socket for the actual communication
+            Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint tcpServerEP = new IPEndPoint(IPAddress.Any, 0);
+            tcpSocket.Bind(tcpServerEP);
+
+            // Get the IP and port of the TCP socket so that we can forward it to the client
+            IPEndPoint iPEndPoint = (IPEndPoint)tcpSocket.LocalEndPoint;
+
+            // Receive the initial message from the client
+            byte[] buffer = new byte[1024];
+            EndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+            int received = initialConnection.ReceiveFrom(buffer, ref clientEP);
+            string message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Received: {message}");
+
+            // Assemble the IP and port of the TCP socket and send it back to the client
+            string response = $"{((IPEndPoint)iPEndPoint).Address.ToString()},{((IPEndPoint)iPEndPoint).Port}";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            initialConnection.SendTo(responseData, clientEP);
+            initialConnection.Close();
+
+            // Accept the TCP connection from the client
+            tcpSocket.Listen(1);
+            Socket acceptedSocket = tcpSocket.Accept();
+
+            // The communication may now ensue, this is just a test
+            byte[] acceptedBuffer = new byte[1024];
+            int receivedBytes = acceptedSocket.Receive(acceptedBuffer);
+            string receivedMessage = Encoding.UTF8.GetString(acceptedBuffer, 0, receivedBytes);
+            Console.WriteLine($"Received: {receivedMessage}");
+            acceptedSocket.Close();
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
